20acc9058d55bd829bff3fd04aca45dad2fd8b1e
G Sumanth Reddy <35569919+gskr442@users.noreply.github.com>
2023-05-14 12:05:37 +0530

    mocha reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'q5rk7b',
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     excludeSpecPattern: '**/2-advanced-examples/.*.js',
   },
